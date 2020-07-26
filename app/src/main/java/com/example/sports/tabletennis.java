ed21995505c4886322b4e71f9df7f3beb7a60993
muskanrani <muskanrani@users.noreply.github.com>
2020-07-26 23:48:40 +0530

    terms and condition

--- app/src/main/java/com/example/sports/tabletennis.java
+++ app/src/main/java/com/example/sports/tabletennis.java
@@ -3,6 +3,7 @@ package com.example.sports;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class tabletennis extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class tabletennis extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabletennis);
+        View ttTerm= findViewById(R.id.tt_Terms);
+        ttTerm.setVisibility(View.GONE);
     }
 }
